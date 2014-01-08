360d148e6d085971ae267b325972124753118c8c
Charles Hughes <c.hughes87@gmail.com>
2014-01-08 15:41:47 -0800

    started functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,23 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  prototype = stackMethods; 
+  return instance;
 };
   
-var stackMethods = {};
+var stackMethods = {
+  instance: {},
+  storage: {},
+  length: 0,
+  push: function(value){
+    storage[length] = value;
+    length++;
+  },
+  pop: function(){
+    var popped = storage[size-1];        
+    storage[length-1] && delete storage[length-1];
+    length && length--;
+    return popped;
+  },
+  size: function(){
+    return length;
+  }
+};
