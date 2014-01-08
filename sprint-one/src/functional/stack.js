167635bb6502aab1ca17428bdb98a8203b72aac6
Charles Hughes <c.hughes87@gmail.com>
2014-01-08 11:44:43 -0800

    made empty stack return zero

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,11 +1,9 @@
 var makeStack = function(){
-    var instance = {};
+  var instance = {};
 
   // Use an object with numeric keys to store values
-    var storage = {};
-  /* START PROMPT
-    var size; // Hint: set an initial value here
-  -END PROMPT */
+  var storage = {};
+  var size = 0;
 
   // Implement the methods below
   instance.push = function(value){
@@ -15,6 +13,7 @@ var makeStack = function(){
   };
 
   instance.size = function(){
+    return size;
   };
   
     return instance;
