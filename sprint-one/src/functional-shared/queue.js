90b975706ea32800639e09914c8062a224a625f2
Hack Reactor Students <c.hughes87@gmail.com>
2014-01-08 15:55:01 -0800

    almost implemented shared queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,29 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {
+    storage: {},
+    start: 0,
+    end: 0,
+    size: 0
+  }
+  return _.extend(instance,queueMethods);
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.end] = value;
+    this.end++;
+    this.size++;
+  },
+  dequeue: function(){
+    if(this.size){
+      this.size--;
+      var result = this.storage[this.start];
+      delete this.storage[this.start];
+      this.start++;
+      return result;
+    }
+  },
+  size: function(){
+    return this.size;
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,25 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = {
+    storage: {},
+    size: 0
+  }
+  return _.extend(instance,stackMethods);
 };
   
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.size] = value;
+    this.size++;
+  },
+  pop: function(){
+    if(this.size){
+      this.size--;
+      var result = this.storage[this.size];
+      delete this.storage[this.size];
+      return result;
+    }
+  },
+  size: function(){
+    return this.size;
+  }
+};
