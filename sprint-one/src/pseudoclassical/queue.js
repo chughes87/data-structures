529f88bb65c0e1b1dd372f55c2d4f47157908eb2
Hack Reactor Students <c.hughes87@gmail.com>
2014-01-08 17:40:19 -0800

    completed pseudoclassical queue and stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,32 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.start = 0;
+  this.end = 0;
+  this.length = 0;
 };
+
+//var makeQueue = function(){
+//  var instance = new Queue();
+//  return instance;
+//};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.end] = value;
+  this.end++;
+  this.length++;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.length){
+    this.length--;
+    var result = this.storage[this.start];
+    delete this.storage[this.start];
+    this.start++;
+    return result;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.length;
+};
+
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,22 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.length = 0;
+};
+
+Stack.prototype.push = function(value){
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+Stack.prototype.pop = function(){
+  if(this.length){
+    this.length--;
+    var result = this.storage[this.length];
+    delete this.storage[this.length];
+    return result;
+  }
+};
+
+Stack.prototype.size = function(){
+  return this.length;
 };
