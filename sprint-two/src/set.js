329435ca13fd725407341f8bcc07b4209cdb262d
Hack Reactor Students <c.hughes87@gmail.com>
2014-01-08 21:39:32 -0800

    completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,16 +1,23 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  var result = false;
+  for(var prop in this._storage){
+    item === prop && (result = true);
+  }
+  return result;
 };
 
 setPrototype.remove = function(item){
+  this._storage[item] && (delete this._storage[item]);
 };
